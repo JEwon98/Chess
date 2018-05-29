f6c25303ae1b3432f74ad02ac526af8643486d7a
qqdtf98 <dltjdals98@naver.com>
2018-05-29 16:03:15 +0000

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -1,29 +1,30 @@
-public class Bishop {
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
+ public class Bishop{
+   int[][] board = new int[8][8];
+   public Bishop(){
+     
+   }
+   public void Bishop_move(int x, int y){
+     int p = 0, q = 0;
+     p = x; q = y;
+     for(;p<=0;p--,q++){
+       if(q<8)
+         System.out.printf("%d %d ", p, q);
+     }
+     p = x; q = y;
+     for(;p>=0;p--,q--){
+       if(q>=0)
+         System.out.printf("%d %d ",p,q);
+     }
+     p = x; q = y;
+     for(;q>=0;p++,q--){
+       if(p<8)
+         System.out.printf("%d %d ",p,q);
+     }
+     p = x; q = y;
+     for(;q<8;p++,q++){
+       if(p<8)
+         System.out,printf("%d %d ",p,q);
+     }
+   }
+ }
+       
