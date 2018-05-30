1a47e965d54429130a540047f3d52c62acbc1805
qqdtf98 <dltjdals98@naver.com>
2018-05-30 13:54:32 +0000

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -5,22 +5,22 @@
    }
    public void Bishop_move(int x, int y){
      int p = 0, q = 0;
-     p = x; q = y;
-     for(;p<=0;p--,q++){
+     p = x-1; q = y+1;
+     for(;p>=0;p--,q++){
        if(q<8)
          System.out.printf("%d %d ", p, q);
      }
-     p = x; q = y;
+     p = x-1; q = y-1;
      for(;p>=0;p--,q--){
        if(q>=0)
          System.out.printf("%d %d ",p,q);
      }
-     p = x; q = y;
+     p = x+1; q = y-1;
      for(;q>=0;p++,q--){
        if(p<8)
          System.out.printf("%d %d ",p,q);
      }
-     p = x; q = y;
+     p = x+1; q = y+1;
      for(;q<8;p++,q++){
        if(p<8)
          System.out,printf("%d %d ",p,q);
