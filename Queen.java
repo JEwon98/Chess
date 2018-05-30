185385658c08bcaf7b83683c8bbd67b3066975f6
qqdtf98 <dltjdals98@naver.com>
2018-05-30 22:58:27 +0900

    Add files via upload

---
+++ Queen.java
@@ -0,0 +1,39 @@
+package kr.ac.cau.mecs.lenerd.chess;
+
+public class Queen {
+	static int[][] board = new int[8][8];
+	public Queen() {
+		
+	}
+	public static void Queen_move(int x, int y) {
+		int p = 0, q = 0;
+	     p = x-1; q = y+1;
+	     for(;p>=0;p--,q++){
+	       if(q<8) 
+	         System.out.printf("%d %d ", p, q);
+	       }
+	     p = x-1; q = y-1;
+	     for(;p>=0;p--,q--){
+	       if(q>=0) 
+	         System.out.printf("%d %d ",p,q);
+	     }
+	     p = x+1; q = y-1;
+	     for(;q>=0;p++,q--){
+	       if(p<8) 
+	         System.out.printf("%d %d ",p,q);
+	     }
+	     p = x+1; q = y+1;
+	     for(;q<8;p++,q++){
+	       if(p<8) 
+	         System.out.printf("%d %d ",p,q);
+	     }
+	     for(int i = 0;i < x;i++) 
+	    	 System.out.printf("%d %d ",p,q);
+	     for(int i = x+1;i<8;i++)
+	    	 System.out.printf("%d %d ",p,q);
+	     for(int j = 0;j < y;j++)
+	    	 System.out.printf("%d %d ",p,q);
+	     for(int j = y+1;j <8;j++)
+	    	 System.out.printf("%d %d ",p,q);
+	}
+}
