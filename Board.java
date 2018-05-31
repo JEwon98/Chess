703127401604b4c927e0a3d4cbab350ce52c727c
JEwon98 <33649813+JEwon98@users.noreply.github.com>
2018-05-31 16:46:13 +0900

    Update Board.java

--- Board.java
+++ Board.java
@@ -3,5 +3,6 @@ package kr.ac.cau.mecs.lenerd.chess;
 //현재 판의 상태 정적 변수
 
 public class Board {
-	public static int[][] board = new int[14][14];
+	public static int[][] board = new int[8][8];
+	public static int getX,getY;
 }
