c1caa474bed44b879ce6c8c8d48667ffc354a33e
JEwon98 <33649813+JEwon98@users.noreply.github.com>
2018-05-31 21:04:10 +0900

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,9 +1,13 @@
 package kr.ac.cau.mecs.lenerd.chess;
 
+import javax.swing.JPanel;
+
 //현재 판의 상태 정적 변수
 
 public class Board {
-	public static int[][] board = new int[8][8];		//일대일 보드 1 일부러 
+	public static JPanel[][] chessBoardSquares = new JPanel[8][8];
+	public static int[][] board = new int[8][8];
 	public static int[][] board2 = new int[14][14];
-	public static int getX,getY;
+	public static int boardX,boardY;
+	public static ImagePanel[] bPawn = new ImagePanel[8];
 }
